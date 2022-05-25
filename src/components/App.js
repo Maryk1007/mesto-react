b3b0f3df8cd59b67f9e847d8f86fc2662b6ba00f
Maria <mary-sberbank@yandex.ru>
2022-05-25 16:58:57 +0300

    декларативное открытие и закрытие попапов

--- src/components/App.js
+++ src/components/App.js
@@ -10,89 +10,47 @@ import FormConfirmDelete from './FormConfirmDelete.js';
 import ImagePopup from './ImagePopup.js';
 
 function App() {
+const [isPopupProfileOpen, setIsPopupProfileOpen] = React.useState(false);
+const [isPopupPhotoOpen, setIsPopupPhotoOpen] = React.useState(false);
+const [isPopupAvatarOpen, setIsPopupAvatarOpen] = React.useState(false);
+
+
+function handleEditProfileClick() {
+  setIsPopupProfileOpen(true);
+    }
+
+    function handleAddPlaceClick() {
+      setIsPopupPhotoOpen(true);
+    }
+
+    function handleEditAvatarClick() {
+      setIsPopupAvatarOpen(true);
+    }
+
+    function closeAllPopups() {
+      setIsPopupProfileOpen(false);
+      setIsPopupPhotoOpen(false);
+      setIsPopupAvatarOpen(false);
+    }
+
   return (
     <div className="page">
       <Header />
-      <Main />
+      <Main onEditProfile={handleEditProfileClick} onAddPlace={handleAddPlaceClick} onEditAvatar={handleEditAvatarClick}/>
       <Footer />
-      <PopupWithForm name="profile" title="Редактировать профиль">
+      <PopupWithForm name="profile" title="Редактировать профиль" isOpen={isPopupProfileOpen} onClose={closeAllPopups}>
         <FormWithProfile/>
       </PopupWithForm>
-    {/* <div className="popup popup_profile">
-      <div className="popup__container">
-        <button className="button-close" type="button" aria-label="кнопка закрытия формы"></button>
-        <h2 className="form__text">Редактировать профиль</h2>
-        <form className="popup__form form" name="formPopupProfile" noValidate autoComplete="off">
-          <div className="form__field">
-            <input className="form__input form__input_field_name" type="text" id="userName" name="name" required minLength="2" maxLength="40" placeholder="Напишите ваше имя"/>
-            <span className="form__field-error" id="error-userName"></span>
-          </div>
-          <div className="form__field">
-            <input className="form__input form__input_field_description" type="text" id="userJob" name="about" required minLength="2" maxLength="200" placeholder="Напишите чем вы занимаетесь"/>
-            <span className="form__field-error" id="error-userJob"></span>
-          </div>
-          <button className="button-save" type="submit">Сохранить</button>
-        </form>
-      </div>
-    </div> */}
-    <PopupWithForm name="edit-avatar" title="Обновить аватар">
+    <PopupWithForm name="edit-avatar" title="Обновить аватар" isOpen={isPopupAvatarOpen} onClose={closeAllPopups}>
         <FormWithAvatar/>
       </PopupWithForm>
-    {/* <div className="popup popup_edit-avatar">
-      <div className="popup__container">
-        <button className="button-close" type="button" aria-label="кнопка закрытия формы"></button>
-        <h2 className="form__text">Обновить аватар</h2>
-        <form className="popup__form form" name="formEditAvatar" noValidate autoComplete="off">
-          <div className="form__field">
-            <input className="form__input form__input_field_link-avatar" type="url" id="avatar" name="avatar" required placeholder="Ссылка на ваше новое фото"/>
-            <span className="form__field-error" id="error-avatar"></span>
-          </div>
-          <button className="button-save" type="submit">Сохранить</button>
-        </form>
-      </div>
-    </div> */}
-    <PopupWithForm name="photo" title="Новое место">
+    <PopupWithForm name="photo" title="Новое место" isOpen={isPopupPhotoOpen} onClose={closeAllPopups}>
         <FormWithPhoto/>
       </PopupWithForm>
-    {/* <div className="popup popup_photo">
-      <div className="popup__container">
-        <button className="button-close" type="button" aria-label="кнопка закрытия формы"></button>
-        <h2 className="form__text">Новое место</h2>
-        <form className="popup__form form" name="formPopupPhoto" noValidate autoComplete="off">
-          <div className="form__field">
-            <input className="form__input form__input_field_title" type="text" id="photoName" name="name" required minLength="2" maxLength="30" placeholder="Название"/>
-            <span className="form__field-error" id ="error-photoName"></span>
-          </div>
-          <div className="form__field">
-            <input className="form__input form__input_field_link" type="url" id="photoLink" name="link" required placeholder="Ссылка на картинку"/>
-          <span className="form__field-error" id="error-photoLink"></span>
-          </div>
-          <button className="button-save" type="submit">Создать</button>
-        </form>
-      </div>
-    </div> */}
     <PopupWithForm name="confirm-delete" title="Вы уверены?">
         <FormConfirmDelete/>
       </PopupWithForm>
-    {/* <div className="popup popup_confirm-delete">
-      <div className="popup__container">
-        <button className="button-close" type="button" aria-label="кнопка закрытия формы"></button>
-        <h2 className="form__text">Вы уверены?</h2>
-        <form className="popup__form form" name="formDeletePhoto">
-          <button className="button-save" type="submit">Да</button>
-        </form>
-      </div>
-    </div> */}
     <ImagePopup/>
-    {/* <div className="popup popup_fullview">
-      <div className="popupfullview">
-        <figure className="popupfullview__image">
-          <img className="popupfullview__picture" src="#" alt="#"/>
-          <figcaption className="popupfullview__caption"></figcaption>
-        </figure>
-        <button className="button-close" type="button" aria-label="кнопка закрытия формы"></button>
-      </div>
-    </div> */}
     <template className="item__template">
       <li className="cards">
         <img className="cards__picture" src="#" alt="#"/>
--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-function PopupWithForm({name, title, children}) {
-    const className = `popup popup_${name}`
+function PopupWithForm({name, title, isOpen, onClose, children}) {
+    const className = `popup popup_${name} ${isOpen ? 'popup_opened' : ''}`
 
     return (
         <div className={className}>
             <div className="popup__container">
-                <button className="button-close" type="button" aria-label="кнопка закрытия формы"></button>
+                <button className="button-close" type="button" aria-label="кнопка закрытия формы" onClick={onClose}></button>
                 <h2 className="form__text">{title}</h2>
                 {children}
             </div>
